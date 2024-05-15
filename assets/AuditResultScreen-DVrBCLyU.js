aa8b4e45240f093e96a9687019b44c3fe42f61ce
toonchaianan <toonchaianan@gmail.com>
2024-05-15 16:27:19 +0700

    Updates

---
+++ assets/AuditResultScreen-DVrBCLyU.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DGcQd1Yb.js";function r(){return e.jsx("div",{children:"AuditResultScreen"})}export{r as default};
---
+++ assets/FactoryResultScreen-PGT7kxDM.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-DGcQd1Yb.js";function t(){return e.jsx("div",{children:"FactoryResultScreen"})}export{t as default};
